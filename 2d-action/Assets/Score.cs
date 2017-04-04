5fd3c1057aad51869f65fdff26fb48f64c67406d
Ryosuke Nakamura <nakaryou1120@gmail.com>
2017-04-04 12:06:50 +0900

    move camera set score make wall

---
+++ 2d-action/Assets/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public GUIText scoreGUI;
+	private int score;
+
+	// シーン開始時に一度だけ呼ばれる関数
+	void Start(){
+		score = 0;
+	}
+
+	// スコアの加算
+	void AddScore(int s){
+		// 現在のスコアをsを足したものに更新
+		score = score + s;
+	}
+
+	// シーン中にフレーム毎に呼ばれる関数
+	void Update () {
+		// スコアの表示
+		scoreGUI = gameObject.GetComponent<GUIText>();
+		scoreGUI.text = "" + score;
+	}
+}
